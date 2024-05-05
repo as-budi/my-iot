d40ee822321a74d554ac84635fd2b665f12ce375
as-budi <agungsetiabudi@ub.ac.id>
2024-05-05 20:46:22 +0700

    create device view (device.blade.php) for each device and edit the routes/web.php

---
+++ resources/views/device.blade.php
@@ -0,0 +1,10 @@
+
+{{-- @dd($device) --}}
+
+@extends('layouts.main')
+@section('container')
+    <h2>{{ $device["name"] }} ({{ $device["id"] }})</h2>
+    <h2>Value: {{ $device["current_value"] }}</h2>
+
+    <a href="/devices">back to Devices</a>
+@endsection
