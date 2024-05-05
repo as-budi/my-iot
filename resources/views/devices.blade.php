bd72dad579eeab1015d950d88499ed93d8d7642a
as-budi <agungsetiabudi@ub.ac.id>
2024-05-05 17:32:47 +0700

    Change the sintax to show the data in device.blade.php with blade sintax ({{  }})

--- resources/views/devices.blade.php
+++ resources/views/devices.blade.php
@@ -8,8 +8,8 @@
 </head>
 <body>
     <h1>Devices</h1>
-    <h2> <?php echo $name; ?> </h2>
-    <h3> <?php echo 'Range value: ' . $min_value . '-' . $max_value . ''; ?> </h3>
-    <p> <?php echo 'Current value: ' . $current_value; ?> </p>
+    <h2> {{ $name }} </h2>
+    <h3> Range value: {{ $min_value }} - {{ $max_value }} </h3>
+    <p> Current value: {{ $current_value }} </p>
 </body>
 </html>
