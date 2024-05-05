f54a4b416b6e8a2626e127d49257179822ee124f
as-budi <agungsetiabudi@ub.ac.id>
2024-05-05 19:03:32 +0700

    apply extending layout to all views

--- resources/views/devices.blade.php
+++ resources/views/devices.blade.php
@@ -1,15 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>my-iot | devices</title>
-</head>
-<body>
+@extends('layouts.main')
+@section('container')
     <h1>Devices</h1>
     <h2> {{ $name }} </h2>
     <h3> Range value: {{ $min_value }} - {{ $max_value }} </h3>
     <p> Current value: {{ $current_value }} </p>
-</body>
-</html>
+@endsection
+
