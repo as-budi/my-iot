ae5e9e556fe93e28fa3f283f639685c01cffd8c3
as-budi <agungsetiabudi@ub.ac.id>
2024-05-05 20:12:16 +0700

    show the dummy data on devices view

--- resources/views/devices.blade.php
+++ resources/views/devices.blade.php
@@ -1,11 +1,34 @@
 
-@dd($devices)
-
 @extends('layouts.main')
 @section('container')
+
     <h1>Devices</h1>
-    <h2> {{ $name }} </h2>
-    <h3> Range value: {{ $min_value }} - {{ $max_value }} </h3>
-    <p> Current value: {{ $current_value }} </p>
+
+    @php
+        $i = 1;
+    @endphp
+    <table border="1" cellpadding="10" cellspacing="0">
+        <td>No</td>
+        <td>ID</td>
+        <td>Device Name</td>
+        <td>Min</td>
+        <td>Max</td>
+        <td>Current Value</td>
+        @foreach($devices as $device)
+            <tr>
+                <td>{{ $i }}</td>
+                <td>
+                    <a href="/devices/{{ $device["id"] }}">{{ $device["id"] }}</a>
+                </td>
+                <td>{{ $device["name"] }}</td>
+                <td>{{ $device["min_value"] }}</td>
+                <td>{{ $device["max_value"] }}</td>
+                <td>{{ $device["current_value"] }}</td>
+            </tr>
+            @php
+                $i++;
+            @endphp
+        @endforeach
+
 @endsection
 
