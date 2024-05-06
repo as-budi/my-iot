0f6c19596a5188d5656c3cad19b6d4b22e8a132e
as-budi <agungsetiabudi@ub.ac.id>
2024-05-06 07:06:02 +0700

    Create DeviceController and move the logic from web.php to DeviceController.php

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -1,8 +1,21 @@
 <?php
 
 namespace App\Http\Controllers;
+use App\Models\Device;
 
 abstract class Controller
 {
-    //
+    public function index(){
+        return view('devices', [
+            "title" => "devices",
+            "devices" => Device::all() 
+        ]);
+    }
+
+    public function show($id){
+        return view('device', [
+            "title" => "device",
+            "device" => Device::find($id)
+        ]);
+    }
 }
